133ba4f11525efbf980b6e575c2bb80392bfa8b6
harshal5-dev <harshalganbote55@gmail.com>
2025-06-27 23:52:25 +0530

    feat: Add creationTime configuration to Docker plugin in multiple POM files

---
+++ scripts/categoryData.sql
@@ -0,0 +1,10 @@
+INSERT INTO public.category (id, name, slug, description, image_url) VALUES
+                                                                         (uuid_generate_v4(), 'Electronics', 'electronics', 'Gadgets, devices and more', 'https://split-cart-bucket.s3.eu-north-1.amazonaws.com/Electronics.jpeg'),
+                                                                         (uuid_generate_v4(), 'Apparel', 'apparel', 'Clothing and fashion items', 'https://split-cart-bucket.s3.eu-north-1.amazonaws.com/Apparel.jpeg'),
+                                                                         (uuid_generate_v4(), 'Home & Kitchen', 'home-kitchen', 'Furniture and home accessories', 'https://split-cart-bucket.s3.eu-north-1.amazonaws.com/Home+and+Kitchen.jpeg'),
+                                                                         (uuid_generate_v4(), 'Books', 'books', 'Fiction, non-fiction, and educational', 'https://split-cart-bucket.s3.eu-north-1.amazonaws.com/Books.jpeg'),
+                                                                         (uuid_generate_v4(), 'Toys & Games', 'toys-games', 'Kids toys, board games, and more', 'https://split-cart-bucket.s3.eu-north-1.amazonaws.com/Toys+and+Games.jpeg'),
+                                                                         (uuid_generate_v4(), 'Sports & Outdoors', 'sports-outdoors', 'Active lifestyle and sports equipment', 'https://split-cart-bucket.s3.eu-north-1.amazonaws.com/Sports+and+Outdoors.jpeg'),
+                                                                         (uuid_generate_v4(), 'Health & Beauty', 'health-beauty', 'Personal care products', 'https://split-cart-bucket.s3.eu-north-1.amazonaws.com/Health+and+Beauty.jpeg'),
+                                                                         (uuid_generate_v4(), 'Automotive', 'automotive', 'Car parts and accessories', 'https://split-cart-bucket.s3.eu-north-1.amazonaws.com/Automotive.jpeg');
+
